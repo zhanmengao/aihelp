0e2861d10a75e1bc1c1e2144b3a443330c132cd6
lan <lan@aladinfun.com>
2024-02-29 22:20:59 +0800

    config

--- config/config.go
+++ config/config.go
@@ -1,15 +1,15 @@
 package config
 
 import (
-	"context"
-	"github.com/zhanmengao/gf/config/icfg"
-	"github.com/zhanmengao/gf/config/icfg/cfgtp"
+	"encoding/json"
+	"github.com/gogf/gf/v2/os/gcfg"
+	"github.com/gogf/gf/v2/os/gctx"
 )
 
 type TGptConfig struct {
-	ApiKey    string `yaml:"api_key"`
-	HttpProxy string `yaml:"http_proxy"`
-	AllProxy  string `yaml:"all_proxy"`
+	ApiKey    string `yaml:"api_key" json:"api_key"`
+	HttpProxy string `yaml:"http_proxy" json:"http_proxy"`
+	AllProxy  string `yaml:"all_proxy" json:"all_proxy"`
 }
 
 var (
@@ -17,12 +17,14 @@ var (
 )
 
 func Init(configFile string) (err error) {
-	yCfg, err := icfg.NewConfig(context.Background(), configFile, cfgtp.YAML)
+	yCfg := gcfg.Instance()
+	yCfg.GetAdapter().(*gcfg.AdapterFile).SetFileName(configFile)
+
+	cfg, err := yCfg.Get(gctx.New(), "gpt")
 	if err != nil {
 		return
 	}
-	err = yCfg.Get("gpt", &GptConfig)
-	if err != nil {
+	if err = json.Unmarshal(cfg.Bytes(), &GptConfig); err != nil {
 		return
 	}
 	return
